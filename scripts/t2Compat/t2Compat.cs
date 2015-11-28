3bed76fbeeba1c3128d38fdf23028628f610593b
Robert MacGregor <ragoradev@gmail.com>
2015-11-28 15:37:57 -0500

    Update t2Compat.cs
    Fix comment file title.

--- scripts/t2Compat/t2Compat.cs
+++ scripts/t2Compat/t2Compat.cs
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// scriptCompat.cs
+// t2Compat.cs
 //
 // Tribes 2 script compatability layer for Torque3D. This compatability
 // layer helps Tribes 2 scripts run on the Torque3D engine via various
