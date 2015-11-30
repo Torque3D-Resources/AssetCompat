aaaa1e45b86be80f0f31135818da0fab21d99cde
Calvin Balke <calvinbalke13 AT GMAIL COM>
2015-11-30 12:22:44 -0800

    Updated genmatlist.py

--- genmatlist.py
+++ genmatlist.py
@@ -6,7 +6,7 @@ tf = open('materials.cs','w')
 for name in f:
 	tf.write('singleton Material( "'+name.split('compat\\base\\textures\\')[-1].upper().replace('.png','').replace('.PNG','').rstrip("\n\r").split('\\')[-1]+'" )\n')
 	tf.write('{\n')
-	tf.write('diffuseMap[0] = "'+name.split('compat\\base\\textures\\')[-1].replace("\\","/").replace('.png','').replace('.PNG','').rstrip("\n\r")+'";\n')
+	tf.write('diffuseMap[0] = "'+name.split('compat\\base\\textures\\')[-1].replace("\\","/").rstrip("\n\r")+'";\n')
 	tf.write('mapTo = "'+name.split('compat\\base\\textures\\')[-1].upper().replace('.png','').replace('.PNG','').rstrip("\n\r").split('\\')[-1]+'";\n')
 	tf.write('\n')
 	tf.write('};\n')
